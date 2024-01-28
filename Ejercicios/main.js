6453b0428eba2fe117a22a800187edeefe210ed1
Kevin Perez <kevinperezws201@gmail.com>
2024-01-27 21:06:59 -0800

    Primer Commit

---
+++ Ejercicios/main.js
@@ -0,0 +1,6 @@
+const {myArray,myNumber, myWebadAdres, user} = require ('./module/myModule')
+//Importar datos
+console.log(myArray) 
+console.log(myNumber)
+console.log(myWebadAdres) 
+console.log(user)
---
+++ Ejercicios/timers.js
@@ -0,0 +1,7 @@
+// setInterval(() => {
+//     console.log("Hello world")
+// },2000)
+
+// setTimeout(()=> {
+//     console.log("hello world")
+// },5000)
